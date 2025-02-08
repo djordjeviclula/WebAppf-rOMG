36b0bb86e596387a875b894cfa7ebc748fa9b925
Djordjevicluka@icloud.com <deine.email@beispiel.com>
2025-02-08 17:37:54 +0100

    Aktualisierung der App

--- MeineWebApp/pages/Bewerbung.py
+++ MeineWebApp/pages/Bewerbung.py
@@ -31,10 +31,8 @@ st.subheader("🎓 Zeugnisse")
 
 # Links von Zeu.
 pdf_files = {
-    "Zeugnis Nr. 1 Fachhochschulreife": "https://f6c82f18-3090-44ca-b3bd-b4593bb23558.filesusr.com/ugd/48e2f2_a12066e1c3624309b974a3473609960e.pdf",
-    "Zeugnis Nr. 2 Einführung in die gymnasiale Oberstufe": "https://f6c82f18-3090-44ca-b3bd-b4593bb23558.filesusr.com/ugd/48e2f2_2ef7591aca5d4618adda0a2ca650b71a.pdf",
-    "Zeugnis Nr. 3 mit IT Note": "https://f6c82f18-3090-44ca-b3bd-b4593bb23558.filesusr.com/ugd/48e2f2_0d8de98a6c54437fbd2f5de8e3e4ac1e.pdf"
-}
+    "Zeugnis Überischt ": "https://f6c82f18-3090-44ca-b3bd-b4593bb23558.filesusr.com/ugd/48e2f2_98a270e662cd4e51bbc152c4f3139b7a.pdf"
+   }
 
 # alles als link darstellen
 for title, pdf_link in pdf_files.items():
